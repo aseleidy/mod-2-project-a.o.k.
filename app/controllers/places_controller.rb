0733922e6966642412dcb3b2544a55486c509e70
thekyle5000 <kylepersonalmail@gmail.com>
2020-05-06 14:15:30 -0500

    placescreate

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,14 +3,35 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
   end 
   
-  def new 
-    @itinerary = Itinerary.find(params[:id])
-    @place = Place.new 
-    @itinerary.places << @place 
+  def new
+    @itinerary = Itinerary.find(params[:itinerary_id])
+    @place = Place.new
   end 
 
-  def create 
+  def create
+    puts "************************************"
+    p params
+    
+    @place = Place.new(place_params)
+    @itinerary = Itinerary.find(itinerary_params[:identify])
+    @place.itineraries << @itinerary
+    @place.destination = @itinerary.destination
+      if @place.save
+        redirect_to itinerary_path(@itinerary)
+      else 
+        render new_itinerary_place_path(@itinerary)
+      end
   end 
+
+  private 
+  
+  def itinerary_params
+    params.require(:place).permit(:identify)
+  end 
+
+  def place_params
+    params.require(:place).permit(:name, :category, :url, :img_url, :destination_id)
+  end
 
 
 end 
