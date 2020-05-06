7a84f2b3f3760aaffd63dacae7375ebaa0a44da2
aseleidy <ash.etemadi@gmail.com>
2020-05-06 10:37:05 -0400

    Destination show page and user edit form

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -11,4 +11,15 @@ class Destination < ApplicationRecord
   def full_info
     "#{city} (#{country})"
   end 
+
+  def five_itineraries 
+    self.itineraries.sample(5) 
+  end 
+
+  def most_popular_places 
+    places_popularity = self.places.sort_by do |place|
+      place.itineraries.count
+    end 
+    places_popularity.last(10)
+  end 
 end
